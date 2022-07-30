3c9644858980b1388f899191111019fc9cd8161a
elenadanchenkomn <elenadanchenkomn@gmail.com>
2022-07-31 00:54:02 +0200

    feat: partially fulfilled the decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
+  const units =  ['zero','one','two','three','four', 'five','six','seven','eight','nine' , 'ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+  const dozens =  ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  const hundred = 'hundred';
+  let numbToStr = number.toString();
+  let numbLength = numbToStr.Length;
+  
+  let str = ' ';
+  let newStr = str.split('');
+ if (number < 20) {
+    str = units[number];
+
+ }
+      return str; 
+      
+
+  if(numbLength === 3) {
+  str = units[parseInt(numbToStr.charAt(0))] + hundred;
+  str += dozens[parseInt(numbToStr.charAt(1))];
+  str += units[parseInt(numbToStr.charAt(2))];
+} else {
+str += units[parseInt(numbToStr.charAt(1))];
+str += dozens[parseInt(numbToStr.charAt(0))]; }
+
+ return newStr
+
+    
   
 }
